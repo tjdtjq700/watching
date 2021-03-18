9d0ff3af7dea18c1f9b1c281bf53f514873a24b0
moonsun <kms931116@gmail.com>
2021-03-18 11:36:20 +0900

    이미지불러오기 view.jsp와 servlet-context.xml에 저장루트 부분 참고해주세요

--- finalPjTest/src/main/java/com/finalPj/testpj/dto/ProductDTO.java
+++ finalPjTest/src/main/java/com/finalPj/testpj/dto/ProductDTO.java
@@ -10,15 +10,14 @@ public class ProductDTO {
 	private String aId;
 	private int pUphit;
 	
-	//dName, dType, dUploadDate에 대한 DTO 추가
-	private String dName;
-	private String dType;
-	private Timestamp dUploadDate;
+	//product와 data 테이블 합침
+	private String pImg;
+	private Timestamp pUploadDate;
 	
 	public ProductDTO() {}
-	
-	public ProductDTO(int pCode, String kCode, String tCode, String pName, String aId, int pUphit, String dName,
-			String dType, Timestamp dUploadDate) {
+
+	public ProductDTO(int pCode, String kCode, String tCode, String pName, String aId, int pUphit, String pImg,
+			Timestamp pUploadDate) {
 		super();
 		this.pCode = pCode;
 		this.kCode = kCode;
@@ -26,72 +25,78 @@ public class ProductDTO {
 		this.pName = pName;
 		this.aId = aId;
 		this.pUphit = pUphit;
-		this.dName = dName;
-		this.dType = dType;
-		this.dUploadDate = dUploadDate;
+		this.pImg = pImg;
+		this.pUploadDate = pUploadDate;
 	}
-	
+
 	public int getpCode() {
 		return pCode;
 	}
+
 	public void setpCode(int pCode) {
 		this.pCode = pCode;
 	}
+
 	public String getkCode() {
 		return kCode;
 	}
+
 	public void setkCode(String kCode) {
 		this.kCode = kCode;
 	}
+
 	public String gettCode() {
 		return tCode;
 	}
+
 	public void settCode(String tCode) {
 		this.tCode = tCode;
 	}
+
 	public String getpName() {
 		return pName;
 	}
+
 	public void setpName(String pName) {
 		this.pName = pName;
 	}
+
 	public String getaId() {
 		return aId;
 	}
+
 	public void setaId(String aId) {
 		this.aId = aId;
 	}
+
 	public int getpUphit() {
 		return pUphit;
 	}
+
 	public void setpUphit(int pUphit) {
 		this.pUphit = pUphit;
 	}
-	public String getdName() {
-		return dName;
+
+	public String getpImg() {
+		return pImg;
 	}
-	public void setdName(String dName) {
-		this.dName = dName;
+
+	public void setpImg(String pImg) {
+		this.pImg = pImg;
 	}
-	public String getdType() {
-		return dType;
+
+	public Timestamp getpUploadDate() {
+		return pUploadDate;
 	}
-	public void setdType(String dType) {
-		this.dType = dType;
-	}
-	public Timestamp getdUploadDate() {
-		return dUploadDate;
-	}
-	public void setdUploadDate(Timestamp dUploadDate) {
-		this.dUploadDate = dUploadDate;
+
+	public void setpUploadDate(Timestamp pUploadDate) {
+		this.pUploadDate = pUploadDate;
 	}
 
 	@Override
 	public String toString() {
 		return "ProductDTO [pCode=" + pCode + ", kCode=" + kCode + ", tCode=" + tCode + ", pName=" + pName + ", aId="
-				+ aId + ", pUphit=" + pUphit + ", dName=" + dName + ", dType=" + dType + ", dUploadDate=" + dUploadDate
-				+ "]";
+				+ aId + ", pUphit=" + pUphit + ", pImg=" + pImg + ", pUploadDate=" + pUploadDate + "]";
 	}
-	
 
 }
