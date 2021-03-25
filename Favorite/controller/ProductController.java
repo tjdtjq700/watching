b42083836a7a2be8519291d2b5fd5e95376c4690
jihyebaek <66791543+jihyebaek@users.noreply.github.com>
2021-03-25 16:04:21 +0900

    Update ProductController.java

--- Favorite/controller/ProductController.java
+++ Favorite/controller/ProductController.java
@@ -24,114 +24,104 @@ import com.watching.service.ProductService;
 public class ProductController {
 	
 	@Inject
-	ProductService service;
+	ProductService productService;
 	
-	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+	@Resource(name="uploadPath")
+	private String uploadPath;
 	
-	// 업로드
-	@RequestMapping(value="/upload", method=RequestMethod.GET)
-	public ModelAndView uploadForm () {
-        
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("upload");
+	//기능 upload(insert),list,delete,update(modify),uphit, detailView
+	
+	//리스트 화면 출력
+	@RequestMapping("list")
+	public String list(Model model) throws Exception{
+
+		model.addAttribute("dtos", productService.list());
 		
-        return mv;
+		return "/admin/list2";
 	}
 	
-	// 업로드
-	@RequestMapping(value="/upload", method=RequestMethod.POST)
-	public String uploadSubmit (ProductDTO dto) {
-	    
-		MultipartFile uploadfile = dto.get();
-		if (uploadfile != null) {
-			String dName = uploadfile.getOriginalFilename();
-			dto.setdName(dName);
-			try {
-				File data = new File(dName);
-				uploadfile.transferTo(dName);
-			} catch (IOException e) {
-				e.printStackTrace();
+	//업로드 화면 출력, 리스트에서 업로드 버튼을 누르면 나오는 화면
+	@RequestMapping("uploadView")
+	public String uploadView(Model model) throws Exception{
+		return "/admin/uploadView";
+	}
+	
+	//uploadView에서 작성후 업로드 버튼을 누르면 실제로 db에 올라가는 서비스
+	@RequestMapping(value="upload", method=RequestMethod.POST)
+	public String upload(@ModelAttribute ProductDTO dto, MultipartFile file) throws Exception{
+	
+		
+		String pImg = file.getOriginalFilename();
+		
+		//임시 디렉토리에 사진을 저장
+		File target = new File(uploadPath, pImg);
+		//위의 파일을 지정된 디렉토리로 복사
+		FileCopyUtils.copy(file.getBytes(), target);
+		//파일이름 지정
+		dto.setpImg(pImg);
+		
+		productService.upload(dto);
+		
+		return "redirect:/admin/list"; //또는 다른 화면
+	}
+	
+	//수정을 위한 detailView
+	@RequestMapping("view")
+	public String view(Model model, @RequestParam("pCode")int pCode) throws Exception{
+
+		model.addAttribute("view", productService.view(pCode));
+		return "/admin/view";
+	}
+	//modifyView
+	@RequestMapping("modifyView")
+	public String modifyView(Model model, @RequestParam("pCode")int pCode) throws Exception{
+		model.addAttribute("view", productService.view(pCode));
+		return "/admin/modifyView";
+	}
+	
+	//modifyView에서 수정버튼 누르면 update되는 서비스
+	@RequestMapping("modify")
+	public String modify(@ModelAttribute ProductDTO dto, MultipartFile file) throws Exception {		
+		String pImg = file.getOriginalFilename();
+		
+		//임시 디렉토리에 사진을 저장
+		File target = new File(uploadPath, pImg);
+		//위의 파일을 지정된 디렉토리로 복사
+		FileCopyUtils.copy(file.getBytes(), target);
+		//파일이름 지정
+		dto.setpImg(pImg);
+		
+		productService.modify(dto);
+		
+		return "redirect:/admin/view/{pCode}";
+	}
+	
+	//삭제는 list에서 삭제버튼 누르면 바로 실행
+	@RequestMapping("delete")
+	public String delete(HttpServletRequest request) throws Exception{
+		String[] pCodes = request.getParameterValues("pCodes");
+		//선택확인
+		for(String i : pCodes)System.out.println(Integer.parseInt(i));
+		for(String i : pCodes) {
+			//파일삭제
+			ProductDTO delDto = productService.view(Integer.parseInt(i));
+			String filePath = uploadPath+delDto.getpImg();
+			System.out.println(filePath);
+			
+			File file = new File(filePath);			
+			if(file.exists()) {	
+				if(file.delete()) {
+					System.out.println("이미지 파일 삭제 완료");
+				}else {
+					System.out.println("이미지 파일 삭제 실패");
+				}
+			}else {
+				System.out.println("파일이 존재하지 않음");
 			}
+			productService.delete(Integer.parseInt(i));			
 		}
-			
-	       return "redirect:/list";
-		}
-	
-	// 목록
-	@RequestMapping("/list")
-	public void list(Model model) throws Exception {
-		
-		List<ProductDTO> list = null;
-		list = service.list();
-		
-		model.addAttribute("list", list);
+		return "redirect:/admin/list";	
 	}
 	
-	// 조회
-	@RequestMapping("/view")
-	public void view(@RequestParam("pCode") int pCode, Model model) throws Exception {
-		
-		ProductDTO dto = service.view(pCode);
-		
-		model.addAttribute("view", dto);
-	}
-	
-	// 수정
-	@RequestMapping(value="/modify", method=RequestMethod.POST)
-	public String modify(ProductDTO dto) throws Exception {
-		
-		service.modify(dto);
-		
-		return "redirect:";
-		
-	}
-	
-	// 삭제
-	@RequestMapping(value="/delete", method=RequestMethod.GET)
-	public String delete(@RequestParam("pCode")int pCode) throws Exception {
-		
-		service.delete(pCode);
-		
-		return "redirect:/admin/list";
-	}
-	
-	// 조회수
-	@RequestMapping(value="/uphit")
-	public void uphit(@RequestParam("pCode")int pCode, Model model) throws Exception {
-		
-		ProductDTO dto = service.view(pCode);
-		
-		model.addAttribute("uphit", dto);
-		
-	}
-	
-	
-	// 업로드, 수정, 삭제와 합칠 수도 있음
-	// 데이터 업로드
-	@RequestMapping(value="/dataUpload", method=RequestMethod.POST)
-	public String dataUpload (MultipartFile dataUpload) {
-			
-		return "redirect:/admin/list";
-			
-	}
-	
-	// 데이터 수정
-	@RequestMapping(value="/dataModify", method=RequestMethod.POST)
-	public String dataModify(ProductDTO dto) throws Exception {
-			
-		service.dataModify(dto);
-			
-		return "redirect:";
-			
-	}
-	
-	// 데이터 삭제
-	@RequestMapping(value="/dataDelete", method=RequestMethod.GET)
-	public String dataDelete(@RequestParam("pCode")int pCode) throws Exception {
-			
-		service.dataDelete(pCode);
-			
-		return "redirect:/admin/list";
-	}
 	
 }
